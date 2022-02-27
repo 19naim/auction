cdfafe3ec9364683b98bfa5642040ceaf953285a
Md Naim hossain arif <thk.naim@gmail.com>
2022-02-27 16:47:40 +0100

    Spring boot Initial commit

---
+++ src/test/java/com/naim/auction/AuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naim.auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
